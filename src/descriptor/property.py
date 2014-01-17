10a9307c020f2e16d67510738233490ce2cf1ca7
Jerry Ma <hongloull@gmail.com>
2014-01-18 00:17:31 +0800

    edit descriptor.property

--- src/descriptor/property.py
+++ src/descriptor/property.py
@@ -1,41 +1,44 @@
-"""
-class bake property()
-"""
-
+"""class bake property()"""
+#x = A()
+#x.attr -> Property().__get__(x.attr, x, A)
+#A.attr -> Property().__get__(A.attr, None, A)
+    
 class Property(object):
-    """
-    """
-    def __init__(self,fget=None,fset=None,fdel=None,fdoc=None):
-        self._fget=fget
-        self._fset=fset
-        self._fdel=fdel
-        self._fdoc=fdoc
+    def __init__(self, fget=None, fset=None, fdel=None, fdoc=None):
+        self._fget = fget
+        self._fset = fset
+        self._fdel = fdel
+        self._fdoc = fdoc
         
-    def __get__(self,instance,owner=None):
+    def __get__(self, instance, owner):
         if self._fget is None:
-            raise AttributeError,'can not get attr'
+            raise AttributeError, 'can not get attr'
         if instance is None:
-            pass
+            return self
+        else:
+            return self._fget(instance)
         
-    def __set__(self,instance,value):
+    def __set__(self, instance, value):
         if self._fset is None:
-            raise AttributeError,'can not set attr'
-        if instance is None:
-            self
+            raise AttributeError, 'can not set attr'
+        # instance won't be None
+        self._fset(instance, value)
             
-    def __delete__(self,instance):
+    def __delete__(self, instance):
         if self._fdel is None:
-            raise AttributeError,'can not set attr'
-        return self._fdel
+            raise AttributeError, 'can not set attr'
+        # instance won't be None
+        return self._fdel()
     
 class A(object):
-    def __init__(self,attr):
+    def __init__(self, attr):
         self._attr = attr
-        
     def getAttr(self):
         return self._attr
-    
-    def setAttr(self,value):
+    def setAttr(self, value):
         self._attr = value
+    attr = Property(getAttr, setAttr)
         
-    attr = Property
+x = A('apple')
+A.attr = 'red apple'
+print(A.attr)
